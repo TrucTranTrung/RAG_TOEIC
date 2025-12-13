bc5d76535ff1cf8a396b5d0e0d66aaad89e18d41
TrucTranTrung <tructran172003@gmail.com>
2025-12-13 15:54:46 +0700

    features: update lis2 lis4

--- src/core/modules/Agent_Part6/Part6.py
+++ src/core/modules/Agent_Part6/Part6.py
@@ -243,7 +243,7 @@ async def main():
     # --- Initialize Gemini 2.5 Flash Model ---
     logger.info("--- Khởi tạo Gemini 2.5 Flash Model ---")
 
-    google_api_key = "AIzaSyCWjJtVscHUG5KY"
+    google_api_key = "AI"
     if not google_api_key:
         print("="*50)
         print("LỖI: Vui lòng đặt biến môi trường GOOGLE_API_KEY để chạy ví dụ này.")
